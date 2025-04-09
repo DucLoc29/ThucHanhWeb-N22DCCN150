93118d6f1aaf6cbfe03ff93603ae349c3cf162cc
Duc Loc <nguyenducloc.dalat@gmail.com>
2025-04-09 09:22:00 +0700

    first commit

---
+++ src/pages/AboutUs/AboutUs.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AboutUs.css";
+
+function AboutUs() {
+    return (
+        <div className="about-us-wrapper">
+            <h1>About us</h1>
+        </div>
+    );
+}
+
+export default AboutUs;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import "./Home.css";
+import NavBar from "../../components/NavBar/NavBar";
+
+function Home() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then((res) => res.json())
+            .then((data) => setPosts(data))
+            .catch((err) => console.error("Lỗi API:", err));
+    }, []);
+
+    return (
+        <div className="home-wrapper">
+            <h1>Home</h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Home;
